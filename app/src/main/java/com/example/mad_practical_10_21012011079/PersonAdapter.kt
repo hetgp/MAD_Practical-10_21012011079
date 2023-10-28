28c317cee8c5a3594942d0a19496afcb75f5859c
student <student@gnu.ac.in>
2023-10-28 09:39:58 +0530

    fg

--- app/src/main/java/com/example/mad_practical_10_21012011079/PersonAdapter.kt
+++ app/src/main/java/com/example/mad_practical_10_21012011079/PersonAdapter.kt
@@ -1,14 +1,26 @@
 package com.example.mad_practical_10_21012011079
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.google.android.material.button.MaterialButton
 
-class PersonAdapter (context: Context,val personArray: ArrayList<Person>):ArrayAdapter<Person>(context,0,personArray) {
+class PersonAdapter(private val context:Context, private val personArray: ArrayList<Person>):ArrayAdapter<Person>(context,0,personArray){
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
         val view = LayoutInflater.from(context).inflate(R.layout.person_item,parent,false)
+
+        view.findViewById<TextView>(R.id.tv_layout_1).text = personArray[position].name
+        view.findViewById<TextView>(R.id.tv_layout_2).text = personArray[position].phoneNo
+        view.findViewById<TextView>(R.id.tv_layout_3).text = personArray[position].emailId
+        view.findViewById<TextView>(R.id.tv_layout_4).text= personArray[position].address
+        view.findViewById<MaterialButton>(R.id.MaterialButton2)
+        Intent(context,MapsActivity::class.java).putExtra("object",personArray[position]).apply { context.startActivity(this) }
         return view
     }
+
 }
